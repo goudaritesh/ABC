29ad695775a7b68e87a6d86889a10ebbdd11d42a
goudaritesh <goudariteshkumar1@gmail.com>
2025-05-07 21:40:28 +0530

    Removed 1st line

--- src/Module01_Basics/print.java
+++ src/Module01_Basics/print.java
@@ -2,7 +2,6 @@ package Module01_Basics;
 
 public class print {
     public static void main(String[] args) {
-        System.out.print("Print");
         System.out.print("Ram");
         System.out.println("this is sout.");
         System.out.println("Git from ritesh.");
