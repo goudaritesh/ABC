3ec0ddff678a4f7dbe5d17f98a988559b62474a3
Om-Prakash-13 <itsomprakashadhikari@gmail.com>
2025-05-07 20:14:00 +0530

    Another commit

--- src/Module01_Basics/print.java
+++ src/Module01_Basics/print.java
@@ -4,5 +4,6 @@ public class print {
     public static void main(String[] args) {
         System.out.print("Print");
         System.out.print("Ram");
+        System.out.println("this is sout.");
     }
 }
